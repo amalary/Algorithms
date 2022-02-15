9812780bc63ef1c3d8f1913c4b9d32b27b4f7b0b
Anthony Malary <amalary@bu.edu>
2022-02-15 12:27:39 -0800

    started optimal solution needs work

--- Algorithms.js
+++ Algorithms.js
@@ -16,6 +16,38 @@ let twoSum = function(nums,target) {
         }
     }
 
-}
+} 
 
 console.log(twoSum(nums,target)); 
+
+
+
+
+
+
+
+// Optimized Approach 
+// Hashmap 
+nums = [1,5,9]
+target = 10; 
+map = {0: 1, 1: 5, 2: 9}
+i = 2; 
+value = 9;
+let complement_pair = 10 - 9 == 1;  
+[0,2];
+map[1]; 
+
+function twoSum(nums,target){
+    map = {}; 
+
+    for(let i = 0; i < nums.length; i++){
+        let value = nums[i];
+        let complement_pair = target - value; 
+        if(map[complement_pair] !== undefined){
+            return [map[complement_pair,i ]]; 
+        }else {
+            map[value] = i; 
+        }
+    }}
+
+    console.log(twoSum(nums,target))
