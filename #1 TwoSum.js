a12c920781bceb83257e4f9065525b7e2a8a9f29
Anthony Malary <amalary@bu.edu>
2022-03-14 17:15:18 -0700

    Finally got the optimied twoSum

--- #1 TwoSum.js
+++ #1 TwoSum.js
@@ -23,15 +23,9 @@ console.log(twoSum(nums,target));
 // Hashmap 
 nums = [1,5,9]
 target = 10; 
-map = {0: 1, 1: 5, 2: 9}
-i = 2; 
-value = 9;
-let complement_pair = 10 - 9 == 1;  
-[0,2];
-map[1]; 
 
 function twoSum(nums,target){
-    map = {}; 
+   let map = {}; 
     for(let i = 0; i < nums.length; i++){
         let value = nums[i];
         let complement_pair = target - value; 
@@ -111,3 +105,63 @@ function twoSum(nums,target){
 }
 
 console.log(twoSum(nums,target)); 
+
+// Brute Force 
+
+// Hash Map
+
+// map = {0:1,1:}
+
+let nums = [2,7,11,15]
+
+let target = 9;
+
+function twoSum(nums,target) { 
+    for(let i = 0; i < nums.length; i++){
+        for(let j = i+1; j < nums.length; j++){
+            if(nums[i] + nums[j] == target){
+                return [i,j]
+            }
+        }
+    }
+};
+
+console.log(twoSum(nums,target)); 
+
+// Optimized 
+
+// Hash Map 
+
+// map = {0:1,1:5,2:9}
+
+// i = 0
+
+// value = 1
+
+// complementaryPair = 10 - 9 = 1
+
+let nums = [1,5,9]
+
+let target = 10
+
+
+function twoSum(nums,target){
+    let map = {};
+    for(let i = 0; i < nums.length; i++){
+        let value = nums[i];
+        let complementPair = target - value; 
+        if(map[complementPair] !== undefined){
+            return [map[complementPair],i];
+        }
+        else{
+            map[value] = i; 
+        }
+    }
+}
+
+console.log(twoSum(nums,target)); 
+
+
+let map = {0:1, 1:5, 2: 9}
+
+console.log(map[3])
